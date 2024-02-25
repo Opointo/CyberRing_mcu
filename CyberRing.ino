9b8124211866eb7e46b8effaaa0bb949449652f5
Jiawens <jiawenliu@bupt.edu.cn>
2024-02-25 19:54:53 +0800

    Use adafruit library

--- CyberRing.ino
+++ CyberRing.ino
@@ -1,10 +1,68 @@
 #include <U8g2lib.h>
 
-#include "Arduino_BMI270_BMM150.h"
+#include "Adafruit_AHRS_Mahony.h"
+#include "Adafruit_AHRS_Madgwick.h"
+#include <Adafruit_AHRS_NXPFusion.h>
+#include <Arduino_BMI270_BMM150.h>
+#include <Wire.h>
 
 #include <SPI.h>
 #include <Wire.h>
 #include <SD.h>
+
+class MyBoschSensor : public BoschSensorClass {
+
+public:
+  MyBoschSensor(TwoWire& wire = Wire)
+    : BoschSensorClass(wire){};
+
+protected:
+  virtual int8_t configure_sensor(struct bmi2_dev* dev) {
+    int8_t rslt;
+    uint8_t sens_list[2] = { BMI2_ACCEL, BMI2_GYRO };
+
+    struct bmi2_int_pin_config int_pin_cfg;
+    int_pin_cfg.pin_type = BMI2_INT1;
+    int_pin_cfg.int_latch = BMI2_INT_NON_LATCH;
+    int_pin_cfg.pin_cfg[0].lvl = BMI2_INT_ACTIVE_HIGH;
+    int_pin_cfg.pin_cfg[0].od = BMI2_INT_PUSH_PULL;
+    int_pin_cfg.pin_cfg[0].output_en = BMI2_INT_OUTPUT_ENABLE;
+    int_pin_cfg.pin_cfg[0].input_en = BMI2_INT_INPUT_DISABLE;
+
+    struct bmi2_sens_config sens_cfg[2];
+    sens_cfg[0].type = BMI2_ACCEL;
+    sens_cfg[0].cfg.acc.bwp = BMI2_ACC_OSR2_AVG2;
+    sens_cfg[0].cfg.acc.odr = BMI2_ACC_ODR_100HZ;
+    sens_cfg[0].cfg.acc.filter_perf = BMI2_PERF_OPT_MODE;
+    sens_cfg[0].cfg.acc.range = BMI2_ACC_RANGE_4G;
+    sens_cfg[1].type = BMI2_GYRO;
+    sens_cfg[1].cfg.gyr.filter_perf = BMI2_PERF_OPT_MODE;
+    sens_cfg[1].cfg.gyr.bwp = BMI2_GYR_OSR2_MODE;
+    sens_cfg[1].cfg.gyr.odr = BMI2_GYR_ODR_100HZ;
+    sens_cfg[1].cfg.gyr.range = BMI2_GYR_RANGE_2000;
+    sens_cfg[1].cfg.gyr.ois_range = BMI2_GYR_OIS_2000;
+
+    rslt = bmi2_set_int_pin_config(&int_pin_cfg, dev);
+    if (rslt != BMI2_OK)
+      return rslt;
+
+    rslt = bmi2_map_data_int(BMI2_DRDY_INT, BMI2_INT1, dev);
+    if (rslt != BMI2_OK)
+      return rslt;
+
+    rslt = bmi2_set_sensor_config(sens_cfg, 2, dev);
+    if (rslt != BMI2_OK)
+      return rslt;
+
+    rslt = bmi2_sensor_enable(sens_list, 2, dev);
+    if (rslt != BMI2_OK)
+      return rslt;
+
+    return rslt;
+  }
+};
+
+MyBoschSensor myIMU(Wire1);
 
 #define PANIC(msg)         \
   {                        \
@@ -21,18 +79,40 @@ U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, U8X8_PIN_NONE);
 
 const int chipSelect = 10;
 
-struct IMUData
-{
-  float accel_x;
-  float accel_y;
-  float accel_z;
-  float gyro_x;
-  float gyro_y;
-  float gyro_z;
-  float mag_x;
-  float mag_y;
-  float mag_z;
-};
+// Mag calibration values are calculated via ahrs_calibration.
+// These values must be determined for each baord/environment.
+// See the image in this sketch folder for the values used
+// below.
+
+// Offsets applied to raw x/y/z mag values
+float mag_offsets[3] = { -20.80F, 10.19F, -18.41 };
+
+// Soft iron error compensation matrix
+float mag_softiron_matrix[3][3] = { { 1.004, -0.006, 0.003 },
+                                    { -0.006, 0.984, -0.004 },
+                                    { 0.003, -0.004, 1.013 } };
+
+float mag_field_strength = 44.75F;
+
+// Offsets applied to compensate for gyro zero-drift error for x/y/z
+// Raw values converted to rad/s based on 250dps sensitiviy (1 lsb = 0.00875 rad/s)
+float rawToDPS = 0.00875F;
+float dpsToRad = 0.017453293F;
+float gyro_zero_offsets[3] = { 0.0F, 0.0F, 0.0F };
+
+
+// Mahony is lighter weight as a filter and should be used
+// on slower systems
+Adafruit_Mahony filter;
+//Adafruit_Madgwick filter;
+//Adafruit_NXPSensorFusion filter;
+
+float accelX, accelY, accelZ,          // units m/s/s i.e. accelZ if often 9.8 (gravity)
+  gyroX, gyroY, gyroZ,                 // units dps (degrees per second)
+  gyroDriftX, gyroDriftY, gyroDriftZ,  // units dps
+  magX, magY, magZ, mag_x, mag_y;
+
+long lastInterval, lastTime, a;
 
 void setup()
 {
@@ -41,8 +121,13 @@ void setup()
     ;
 
   Serial.print("\n等着 \n");
-  RASSERT(IMU.begin(), "你IMU炸了");
+  myIMU.begin();
   RASSERT(SD.begin(chipSelect), "你SD卡死了");
+
+  calibrateIMU(250, 250);
+  lastTime = micros();
+
+  filter.begin(25);
 
   // u8g2.setI2CAddress(0x78);
   // ASSERT(u8g2.begin(), "你屏幕烧了");
@@ -51,8 +136,6 @@ void setup()
 
 void loop()
 {
-  static IMUData imu_d = {0};
-  static unsigned long last_report_time = 0; // 上次输出时间
   static unsigned int file_count = 0;
   static SDLib::File myFile;
   // millis() 每50天溢出一次
@@ -67,60 +150,78 @@ void loop()
     myFile = SD.open(filename, FILE_WRITE); // [文件不存在的话会自动创建](https://www.arduino.cc/reference/en/libraries/sd/open/)。
     RASSERT(myFile, "md打不开");
   }
-  if (readIMU(&imu_d))
-  {
-    Serial.print("Accel:");
-    Serial.print(imu_d.accel_x);
-    Serial.print(",");
-    Serial.print(imu_d.accel_y);
-    Serial.print(",");
-    Serial.print(imu_d.accel_z);
-    Serial.print("  Gyro:");
-    Serial.print(imu_d.gyro_x);
-    Serial.print(",");
-    Serial.print(imu_d.gyro_y);
-    Serial.print(",");
-    Serial.print(imu_d.gyro_z);
-    Serial.print("  Mag:");
-    Serial.print(imu_d.mag_x);
-    Serial.print(",");
-    Serial.print(imu_d.mag_y);
-    Serial.print(",");
-    Serial.print(imu_d.mag_z);
 
-    myFile.print(imu_d.accel_x);
+readIMU();
+  unsigned long currentTime = micros();
+  lastInterval = currentTime - lastTime;  // expecting this to be ~104Hz +- 4%
+  lastTime = currentTime;
+
+  // Apply mag offset compensation (base values in uTesla)
+  float x = magX - mag_offsets[0];
+  float y = magY - mag_offsets[1];
+  float z = magZ - mag_offsets[2];
+
+  // Apply mag soft iron error compensation
+  float mx = x * mag_softiron_matrix[0][0] + y * mag_softiron_matrix[0][1] + z * mag_softiron_matrix[0][2];
+  float my = x * mag_softiron_matrix[1][0] + y * mag_softiron_matrix[1][1] + z * mag_softiron_matrix[1][2];
+  float mz = x * mag_softiron_matrix[2][0] + y * mag_softiron_matrix[2][1] + z * mag_softiron_matrix[2][2];
+
+  // Apply gyro zero-rate error compensation
+  float gx = gyroX - gyro_zero_offsets[0];
+  float gy = gyroY - gyro_zero_offsets[1];
+  float gz = gyroZ - gyro_zero_offsets[2];
+
+  // Update the filter
+  filter.update(gx * dpsToRad, gy * dpsToRad, gz * dpsToRad,
+                accelX, accelY, accelZ,
+                mx, my, mz);
+
+  // Print the orientation filter output
+  if (millis() - a >= 250) {
+    a += 250;
+    float roll = filter.getRoll();
+    float pitch = filter.getPitch();
+    float heading = filter.getYaw();
+    
+    myFile.print(accelX);
     myFile.print(",");
-    myFile.print(imu_d.accel_y);
+    myFile.print(accelY);
     myFile.print(",");
-    myFile.print(imu_d.accel_z);
+    myFile.print(accelZ);
     myFile.print(",");
-    myFile.print(imu_d.gyro_x);
+    myFile.print(gyroX);
     myFile.print(",");
-    myFile.print(imu_d.gyro_y);
+    myFile.print(gyroY);
     myFile.print(",");
-    myFile.print(imu_d.gyro_z);
+    myFile.print(gyroZ);
     myFile.print(",");
-    myFile.print(imu_d.mag_x);
+    myFile.print(magX);
     myFile.print(",");
-    myFile.print(imu_d.mag_y);
+    myFile.print(magY);
     myFile.print(",");
-    myFile.print(imu_d.mag_z);
-    unsigned long interval; // 本次输出所用时间
-    // micros() 最长记录70分钟，超过会溢出
-    // 此处最好能暂停 micros(), 不然可能遇到奇怪的边界问题
-    if (micros() > last_report_time)
-    {
-      interval = micros() - last_report_time;
-    }
-    else
-    {
-      interval = micros() + (0xffffffff - last_report_time);
-    }
-    last_report_time = micros();
-    Serial.print(",");
-    Serial.println(interval);
+    myFile.print(magZ);
     myFile.print(",");
-    myFile.println(interval);
+    myFile.print(roll);
+    myFile.print(",");
+    myFile.print(pitch);
+    myFile.print(",");
+    myFile.println(heading);
+    /*
+    // Print the orientation filter output in quaternions.
+    // This avoids the gimbal lock problem with Euler angles when you get
+    // close to 180 degrees (causing the model to rotate or flip, etc.)
+   float qw, qx, qy, qz;
+    filter.getQuaternion(&qw, &qx, &qy, &qz);
+    Serial.print(millis());
+    Serial.print(" - Quat: ");
+    Serial.print(qw);
+    Serial.print(" ");
+    Serial.print(qx);
+    Serial.print(" ");
+    Serial.print(qy);
+    Serial.print(" ");
+    Serial.println(qz);
+ */
   }
   // u8g2.clearBuffer();
   // u8g2.drawStr(0, 10, "I AM FUCKING ALIVE");
@@ -133,17 +234,34 @@ void loop()
   }
 }
 
-int readIMU(IMUData *d)
-{
-  if (IMU.accelerationAvailable() && IMU.gyroscopeAvailable())
-  {
-    IMU.readAcceleration(d->accel_x, d->accel_y, d->accel_z);
-    IMU.readGyroscope(d->gyro_x, d->gyro_y, d->gyro_z);
-    if (IMU.magneticFieldAvailable())
-    {
-      IMU.readMagneticField(d->mag_x, d->mag_y, d->mag_z);
-    }
-    return 1;
+void readIMU() {
+  myIMU.readAcceleration(accelX, accelY, accelZ);
+  myIMU.readGyroscope(gyroX, gyroY, gyroZ);
+  myIMU.readMagneticField(magX, magY, magZ);
+}
+
+void calibrateIMU(int delayMillis, int calibrationMillis) {
+
+  int calibrationCount = 0;
+
+  delay(delayMillis);  // to avoid shakes after pressing reset button
+
+  float sumX, sumY, sumZ;
+  int startTime = millis();
+  while (millis() < startTime + calibrationMillis) {
+    readIMU();
+    // in an ideal world gyroX/Y/Z == 0, anything higher or lower represents drift
+    sumX += gyroX;
+    sumY += gyroY;
+    sumZ += gyroZ;
+    calibrationCount++;
   }
-  return 0;
+
+  if (calibrationCount == 0) {
+    Serial.println("Failed to calibrate");
+  }
+
+  gyroDriftX = sumX / calibrationCount;
+  gyroDriftY = sumY / calibrationCount;
+  gyroDriftZ = sumZ / calibrationCount;
 }
